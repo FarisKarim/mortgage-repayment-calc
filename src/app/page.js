1f65ac9c0c72b0dbbfcdc31d38584068a11aec20
Faris <fariskarim2000@gmail.com>
2024-06-30 12:45:25 -0400

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -15,13 +15,32 @@ export default function Home() {
   const [monthlyRepayments, setMonthlyRepayments] = useState(null);
   const [totalRepayments, setTotalRepayments] = useState(null);
   const [calculationComplete, setCalculationComplete] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState({});
 
   const handleCalculate = () => {
     const parsedMortgageAmount = parseFloat(mortgageAmount.replace(/,/g, ""));
-    if (!parsedMortgageAmount || !mortgageTerm || !interestRate) {
+    const newErrors = {};
+
+    if (!parsedMortgageAmount) {
+      newErrors.mortgageAmount = "This field is required.";
+    }
+
+    if (!mortgageType) {
+      newErrors.mortgageType = "This field is required."
+    }
+
+    if (!mortgageTerm) {
+      newErrors.mortgageTerm = "This field is required.";
+    }
+
+    if (!interestRate) {
+      newErrors.interestRate = "This field is required.";
+    }
+
+    setError(newErrors);
+
+    if (Object.values(newErrors).some((error) => error !== "")) {
       setCalculationComplete(false);
-      setError("All fields are required.");
       return;
     }
 
@@ -30,7 +49,13 @@ export default function Home() {
     const monthlyInterestRate = parseFloat(interestRate) / 100 / 12;
 
     if (isNaN(principal) || isNaN(years) || isNaN(monthlyInterestRate)) {
-      setError("Please enter valid numbers.");
+      setError({
+        mortgageAmount: isNaN(principal) ? "Please enter a valid number." : "",
+        mortgageTerm: isNaN(years) ? "Please enter a valid number." : "",
+        interestRate: isNaN(monthlyInterestRate)
+          ? "Please enter a valid number."
+          : "",
+      });
       setCalculationComplete(false);
       return;
     }
@@ -51,7 +76,6 @@ export default function Home() {
 
     setMonthlyRepayments(monthlyRepayment.toFixed(2));
     setTotalRepayments(totalRepayment.toFixed(2));
-    setError("");
     setCalculationComplete(true);
   };
 
@@ -63,63 +87,75 @@ export default function Home() {
   };
 
   return (
-    <main className="flex h-screen bg-slate-100 items-center justify-center">
-      <div className="flex flex-col sm:flex-row bg-white rounded-2xl w-[100vw] sm:w-1/2 h-auto">
-        <div className="w-1/2 p-8">
-          <div className="flex mb-10 justify-between">
-            <p className="text-xl font-bold">Mortgage Calculator</p>
-            <button className="hover:underline accent-slate-500 text-sm text-slate-500">
+    <main className="flex sm:h-screen bg-slate-100 items-center overflow-scroll justify-center">
+      <div className="flex p-2 sm:p-0 flex-col sm:flex-row bg-white overflow-scroll rounded-2xl w-[100vw] sm:w-1/2 h-auto">
+        <div className="sm:w-1/2 p-2 sm:p-8">
+          <div className="flex flex-col sm:flex-row sm:mb-10 justify-between">
+            <p className="text-xl mb-1 sm:mb-0 font-bold">Mortgage Calculator</p>
+            <button
+              onClick={() => {
+                setMortgageAmount("");
+                setMortgageTerm("");
+                setInterestRate("");
+                setMonthlyRepayments(null);
+                setTotalRepayments(null);
+                setMortgageType("")
+                setError({});
+                setCalculationComplete(false);
+              }}
+              className="underline accent-slate-500 text-sm text-start mb-4 sm:mb-0 text-slate-500"
+            >
               Clear All
             </button>
           </div>
           <p className="text-sm text-slate-500 mb-1">Mortgage Amount</p>
-          <div className="flex w-full border border-slate-500 mb-1 hover:border-black rounded-lg">
-            <div className="w-12 text-center text-slate-500 text-lg p-2 rounded-tl-lg rounded-bl-lg bg-slate-100">
+          <div className={`flex w-full border mb-1 focus:border-none focus-within:ring focus-within:ring-lime hover:border-black rounded-lg ${error.mortgageAmount ? "border-red" : "border-slate-500"} `}>
+            <div className={`w-12 text-center text-lg p-2 rounded-tl-lg rounded-bl-lg ${error.mortgageAmount ? "bg-red text-white" : "bg-slate-100 text-slate-700"}`}>
               £
             </div>
             <input
               value={mortgageAmount}
               onChange={(e) => setMortgageAmount(e.target.value)}
-              className="w-full px-3 rounded-br-lg rounded-tr-lg h-11"
+              className="w-full px-3 rounded-br-lg focus:outline-none focus:border-lime rounded-tr-lg h-11"
             />
           </div>
-          <div className="h-4 mb-4">
-            {error && <p className="text-xs text-red">{error}</p>}
+          <div className="h-4 sm:mb-4">
+            {error.mortgageAmount && <p className="text-xs text-red">{error.mortgageAmount}</p>}
           </div>
-          <div className="flex gap-2 justify-between">
-            <div className="flex w-1/2 flex-col">
-              <p className="text-sm text-slate-500 mb-1">Mortage Term</p>
-              <div className="flex border border-slate-500 rounded-lg hover:border-slate-900">
+          <div className="flex flex-col sm:flex-row gap-2 justify-between">
+            <div className="flex w-full sm:w-1/2 flex-col">
+              <p className="text-sm text-slate-500 mb-1">Mortgage Term</p>
+              <div className={`flex border rounded-lg hover:border-slate-900 ${error.mortgageTerm ? "border-red" : 'border-slate-500'}`}>
                 <input
                   value={mortgageTerm}
                   onChange={(e) => setMortgageTerm(e.target.value)}
-                  className="hover:border-slate-900 px-3  w-3/5 rounded-tl-lg rounded-bl-lg h-11"
+                  className="hover:border-slate-900 px-3 focus:outline-none w-3/5 rounded-tl-lg rounded-bl-lg h-11"
                 />
-                <div className="flex w-2/5 bg-slate-100  rounded-tr-lg rounded-br-lg p-2 justify-center items-center">
-                  <div className="text-sm text-slate-700">years</div>
+                <div className={`flex w-2/5 rounded-tr-lg rounded-br-lg p-2 justify-center  items-center ${error.mortgageTerm ? "bg-red" : "bg-slate-100"}`}>
+                  <div className={`text-sm ${error.mortgageTerm ? 'text-white' : 'text-slate-700'}`}>years</div>
                 </div>
               </div>
-              {error && (
+              {error.mortgageTerm && (
                 <div className="h-4 mt-1">
-                  <p className="text-xs text-red">{error}</p>
+                  <p className="text-xs text-red">{error.mortgageTerm}</p>
                 </div>
               )}
             </div>
-            <div className="flex w-1/2 flex-col">
-              <p className="text-sm text-slate-500 mb-1">Interest Rate</p>
-              <div className="flex border-slate-500 rounded-lg border hover:border-slate-900">
+            <div className="flex w-full sm:w-1/2 flex-col">
+              <p className="text-sm text-slate-500 mb-1 ">Interest Rate</p>
+              <div className={`flex rounded-lg border  hover:border-slate-900 ${error.interestRate ? 'border-red' : 'border-slate-500'} `}>
                 <input
                   value={interestRate}
                   onChange={(e) => setInterestRate(e.target.value)}
-                  className="w-4/5 px-3 rounded-tl-lg rounded-bl-lg h-11"
+                  className="w-4/5 px-3 rounded-tl-lg rounded-bl-lg focus:outline-none h-11"
                 />
-                <div className="flex w-1/5 bg-slate-100 p-2 justify-center rounded-tr-lg rounded-br-lg items-center">
-                  <div className="text-sm text-slate-500">%</div>
+                <div className={`flex w-1/5 p-2 justify-center rounded-tr-lg rounded-br-lg items-center  ${error.interestRate ? "bg-red" : "bg-slate-100"}`}>
+                  <div className={`text-sm text-slate-500 ${error.interestRate ? 'text-white' : 'text-slate-700'}`}>%</div>
                 </div>
               </div>
-              {error && (
+              {error.interestRate && (
                 <div className="h-4 mt-1">
-                  <p className="text-xs text-red">{error}</p>
+                  <p className="text-xs text-red">{error.interestRate}</p>
                 </div>
               )}
             </div>
@@ -133,7 +169,7 @@ export default function Home() {
               mortgageType === "repayment" ? "border-lime bg-yellow-100" : ""
             }`}
           >
-            <div className="rounded-full border-black border h-4 w-4"></div>
+            <div className={`rounded-full border-black border h-4 w-4 ${mortgageType == 'repayment' && ' bg-lime'}`}></div>
             <p className="pl-6">Repayment</p>
           </button>
           <button
@@ -144,17 +180,16 @@ export default function Home() {
                 : ""
             } `}
           >
-            <div className="rounded-full border-black border h-4 w-4"></div>
+            <div className={`rounded-full border-black border h-4 w-4 ${mortgageType == 'interest-only' && ' bg-lime'} `}></div>
             <p className="pl-6">Interest Only</p>
-            
           </button>
-          <div className="h-4 mt-1 mb-4">
-            {error && <p className="text-xs text-red">{error}</p>}
+          <div className="h-4 mt-1 sm:mb-4">
+            {error.mortgageType && <p className="text-xs text-red">{error.mortgageType}</p>}
           </div>
-          
+
           <button
             onClick={handleCalculate}
-            className="w-auto px-6 flex justify-center items-center gap-2 text-black bg-lime rounded-full font-bold py-3"
+            className="w-full sm:w-auto px-6 flex justify-center items-center gap-2 text-black bg-lime rounded-full font-bold py-3"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -172,7 +207,7 @@ export default function Home() {
           </button>
         </div>
         {calculationComplete ? (
-          <div className="w-1/2 flex flex-col gap-4 p-8 bg-slate-900 rounded-tr-2xl rounded-br-2xl rounded-bl-4xl">
+          <div className="sm:w-1/2 flex flex-col gap-4 p-8 bg-slate-900 rounded-tr-2xl rounded-br-2xl rounded-bl-4xl">
             <p className="text-white font-bold text-xl">Your results</p>
             <p className="text-xs leading-relaxed mb-4 text-slate-300">
               Your results are shown below based on the information you
@@ -194,7 +229,7 @@ export default function Home() {
             </div>
           </div>
         ) : (
-          <div className="w-1/2 flex justify-center items-center flex-col gap-3 px-4 bg-slate-900 rounded-bl-4xl">
+          <div className="sm:w-1/2 flex justify-center items-center flex-col gap-3 p-8 sm:px-4 sm:py-0 bg-slate-900 rounded-bl-4xl">
             <Image
               className="object-cover -mt-4"
               src={illustration}
